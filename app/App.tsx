4f56bd47d273ed6385e173106f3304251dcf8cc9
Gabriel Loutfalla Kanawati <gabrielkanawati3@gmail.com>
2022-11-20 22:53:29 -0300

    Waiter-App - JStack - App - Setup app mobile

---
+++ app/App.tsx
@@ -0,0 +1,34 @@
+import { useFonts } from 'expo-font';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Loading } from './src/components/Loading';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+    'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontFamily: 'GeneralSans-700' }}>
+        Open up App.tsx to start working on your app!
+      </Text>
+      <StatusBar style='auto' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
